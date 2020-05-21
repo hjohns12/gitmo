14372c5f368cd30a2971f54a9bac3252731d6c24
Hope Johnson <hopecaneel@gmail.com>
2020-05-20 20:58:21 -0400

    promise ALL

--- index.js
+++ index.js
@@ -18,30 +18,25 @@ let state = {
     selPop: null,
 };
 
-//read in data (by year+news source)
-d3.csv("./data/media_occurences_wide.csv", d3.autoType)
-  .then(data => {
-    state.data = data;
-    const series = d3.stack()
-      .keys(data.columns.slice(1))(data)
-      .map(d => (d.forEach(v => v.key = d.key), d))
-    state.series = series
-    })
-// read in data (by day)
-d3.csv("./data/media_by_date.csv", d => ({
-  date: parser(d.date),
-  count: +d.count,
-})).then(raw_data => {
-  state.lineData = raw_data;
-});
-// read in data re: nDetained by year
-d3.csv("./data/history.csv", d => ({
-  year: +d.year,
-  value: +d.nDetained,
-})).then(hist_data => {
-  state.historicalData = hist_data;
+//read in all data
+Promise.all([
+  d3.csv("./data/media_occurences_wide.csv", d3.autoType),
+  d3.csv("./data/media_by_date.csv", d => ({
+    date: parser(d.date),
+    count: +d.count })),
+  d3.csv("./data/history.csv", d => ({
+      year: +d.year,
+      value: +d.nDetained })),
+]).then(data => {
+  state.data = data[0];
+  const series = d3.stack()
+    .keys(data[0].columns.slice(1))(data[0])
+    .map(d => (d.forEach(v => v.key = d.key), d))
+  state.series = series;
+  state.lineData = data[1];
+  state.historicalData = data[2];
   init();
-});
+})
 
 function init() {
   barchart = new Barchart(state, setGlobalState);
